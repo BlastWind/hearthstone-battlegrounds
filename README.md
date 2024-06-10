abf6ac144943d287d223cb0c029d5a6559bcc25f
flober <achen@clearblade.com>
2024-06-10 16:17:34 -0400

    Added dummy events, dummy gameloop

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Early Work In Progress!
 
 Overall, the architecture is MVC (Model, View, Controller) and some event-driven programming. See discussions on architecture [here](app/Model.md).
+
+### Roadmap
+##### 1. Establish the battlegrounds infrastructure.
+1. Establish turn and gold system.
+2. Implement shop phase. Support dummy minion in shop (one dummy minion for each tier). Randomness implemented. Shared pool not implemented.
+3. Implement combat phase. Currently, just have an AI with a dummy minion.
--- app/Model.hs
+++ app/Model.hs
@@ -1,70 +1,100 @@
-module Model (CardName) where
-    import Control.Monad.State.Lazy (State)
-    {- 
-    Design Philosophy: 
-    The internals of battlegrounds can be modelled as an algebra on GameState.
-    -}
+module Model (GameState) where
+import Control.Monad.State.Lazy (State)
+import Data.Map (Map, empty)
+{- 
+Design Philosophy: 
+The internals of battlegrounds can be modelled as an algebra on GameState.
+-}
 
-    -- ##### BEGIN: Enums ##### 
-    data CardName = AnnoyOTron | BackStageSecurity | CordPuller | DeckSwabbie | DeepSeaAngler | GlimGuardian | HarmlessBonehead | Manasaber | Peggy
-    data Tribe = Quilboar | Pirate | Mech | Elemental | Dragon | Naga | Undead | Beast
-    -- ##### END:   Enums #####
+-- ##### BEGIN: Enums ##### 
+data CardName = AnnoyOTron | BackStageSecurity | CordPuller | DeckSwabbie | DeepSeaAngler | GlimGuardian | HarmlessBonehead | Manasaber | Peggy
+data Tribe = Quilboar | Pirate | Mech | Elemental | Dragon | Naga | Undead | Beast
+-- ##### END:   Enums #####
 
-    data Card = Card { 
-        cardName :: CardName, 
-        tier :: TavernTier, 
-        tribe :: Tribe,
-        attack :: Attack, 
-        health :: Health, 
-        battlecry :: GameState -> GameState, 
-        deathrattle :: GameState -> GameState,
-        onSell :: GameState -> GameState,
-        onBuy  :: GameState -> GameState,
-        counter :: State Int Int -- for seafarer, malchezaar, chimera, etc.
+data Card = Card { 
+    cardName :: CardName, 
+    tier :: TavernTier, 
+    tribe :: Tribe,
+    attack :: Attack, 
+    health :: Health, 
+    battlecry :: GameState -> GameState, 
+    deathrattle :: GameState -> GameState,
+    onSell :: GameState -> GameState,
+    onBuy  :: GameState -> GameState
+    -- counter :: State Int Int -- for seafarer, malchezaar, chimera, etc.
 
-        -- Some cards have very special effects when a tangential action occurs
-        -- Examples: 
-        -- Peggy buffs minion when any card is added to hand
-        -- Swampstriker is buffed when murloc is player
-        -- Seafarer, Malchezaar, Bazaar Dealer needs a counter for its effects
-        -- In combat, chimera, skyblazer can buff minions permanently
-    }
+    -- Some cards have very special effects when a tangential action occurs
+    -- Examples: 
+    -- Peggy buffs minion when any card is added to hand
+    -- Swampstriker is buffed when murloc is player
+    -- Seafarer, Malchezaar, Bazaar Dealer needs a counter for its effects
+    -- In combat, chimera, skyblazer can buff minions permanently
+}
 
-    gainGold :: Int -> (GameState -> GameState)
-    gainGold = undefined
-    -- gainGold i gs  = GameState { ...gs, gold = gold + i }
-
-
-    peggy :: Card
-    peggy = Card {
-        cardName = Peggy,
-        tier = TavernTier 4,
-        attack = Attack 4,
-        health = Health 2,
-        battlecry = id,
-        deathrattle = id,
-        onSell = gainGold 1
-    }
-    
-    newtype Attack = Attack Int
-    newtype Health = Health Int
-    newtype UpgradeCost = UpgradeCost Int
-    newtype TavernTier = TavernTier Int
-    newtype Gold = Gold Int
-    newtype Hand = Hand [Card]
-    newtype Shop = Shop [Card]
-    newtype Board = Board [Card]
-    newtype PlayerHP = PlayerHP Int
+gainGold :: Int -> (GameState -> GameState)
+gainGold = undefined
+-- gainGold i gs  = GameState { ...gs, gold = gold + i }
 
 
-    data PlayerAction = Buy | Sell | Reroll | Freeze | Upgrade
+peggy :: Card
+peggy = Card {
+    cardName = Peggy,
+    tier = TavernTier 4,
+    attack = Attack 4,
+    health = Health 2,
+    battlecry = id,
+    deathrattle = id,
+    onSell = gainGold 1
+}
 
-    data GameState = GameState { 
-        upgradeCost :: UpgradeCost, 
-        tavernTier :: TavernTier, 
-        playerHP :: PlayerHP, 
-        hand :: Hand, 
-        shop :: Shop, 
-        board :: Board, 
-        gold :: Gold 
-    }
+newtype Attack = Attack Int
+newtype Health = Health Int
+newtype UpgradeCost = UpgradeCost Int
+newtype TavernTier = TavernTier Int
+newtype Gold = Gold Int
+newtype Hand = Hand [Card]
+newtype Shop = Shop [Card]
+newtype Board = Board [Card]
+newtype PlayerHP = PlayerHP Int
+newtype Turn = Turn Int -- What turn are we on?
+newtype UserName = UserName String
+
+data GameState = GameState { 
+    turn :: Turn,
+    players :: Map UserName PlayerState
+}
+
+data PlayerState = PlayerState {
+    upgradeCost :: UpgradeCost, 
+    tavernTier :: TavernTier, 
+    playerHP :: PlayerHP, 
+    hand :: Hand, 
+    shop :: Shop, 
+    board :: Board, 
+    curGold :: Gold,
+    maxGold :: Gold,
+    -- implementational
+    eventHandler :: EventHandler
+}
+
+initialGameState :: GameState
+initialGameState = undefined
+
+
+data Event
+    = HealthChange
+    | CardAdded
+    | Refresh
+    | Sell
+    | Buy
+    | Reroll
+    | Freeze
+    | Upgrade
+
+
+type EventHandler = Map Event [GameState -> GameState]
+
+initialHandler :: EventHandler
+initialHandler = empty
+
+game = until onePlayerAlive (combatPhase . shopPhase) initialGameState
